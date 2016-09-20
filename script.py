950ae1d6ee30c8796aa678b2698453a82045c06b
HichamZouarhi <hicham.zouarhi@gmail.com>
2016-09-20 12:24:16 +0100

    automate rasterization and polygonization of the points and diagram

--- script.py
+++ script.py
@@ -2,9 +2,15 @@ from qgis.core import *
 from osgeo import gdal
 import math
 import numpy as np
+import os
 
+#Get the points vector layer
 pointsVector=QgsVectorLayer('./Desktop/PyQGIS_Tests/Weighted Voronoi/points.shp', 'points', 'ogr')
+#Add the vector layer to the map layer registry
 QgsMapLayerRegistry.instance().addMapLayer(pointsVector)
+
+os.system('gdal_rasterize -a z -ts 1000 1000 -l points "/home/user/Desktop/PyQGIS_Tests/Weighted Voronoi/points.shp" "/home/user/Desktop/PyQGIS_Tests/Weighted Voronoi/rasterPoints"')
+
 
 rasterPoints=QgsRasterLayer('./Desktop/PyQGIS_Tests/Weighted Voronoi/rasterPoints', 'rasterPoints')
 QgsMapLayerRegistry.instance().addMapLayer(rasterPoints)
@@ -20,13 +26,13 @@ print "get the points with their weights from raster"
 for row in range(0,height):
 	for col in range(0,width):
 		if(numpy_array[row,col]!=0):
-			#print numpy_array[row,col]
+			print numpy_array[row,col]
 			points.append([row, col, numpy_array[row,col]])
 
 
 
-print "compute the weighted distance grid for each point"
-distanceGrids=[]
+# print "compute the weighted distance grid for each point"
+
 distanceGrid=np.zeros(shape=(height, width))
 for row in range(0,height):
 	for col in range(0,width):
@@ -39,15 +45,28 @@ for row in range(0,height):
 				index=i
 		distanceGrid[row,col]=index
 
+#save the distance grd as an output raster
+#output file name ( path to where to save the raster file )
 outFileName='./Desktop/PyQGIS_Tests/Weighted Voronoi/rasterVoronoi.tiff'
+#call the driver for the chosen format from GDAL
 driver=gdal.GetDriverByName('GTiff')
+#Create the file with dimensions of the input raster ( rasterized points )
 output=driver.Create(outFileName, height, width, 1, gdal.GDT_Byte)
+#set the Raster transformation of the resulting raster
 output.SetGeoTransform(dataset.GetGeoTransform())
+#set the projection of the resulting raster
 output.SetProjection(dataset.GetProjection())
+#insert data to the resulting raster in band 1 from the weighted distance grid
 output.GetRasterBand(1).WriteArray(distanceGrid)
-rasterVoronoi=QgsRasterLayer('./Desktop/PyQGIS_Tests/Weighted Voronoi/rasterVoronoi.tiff')
+#Call the raster output file
+rasterVoronoi=QgsRasterLayer('./Desktop/PyQGIS_Tests/Weighted Voronoi/rasterVoronoi.tiff', 'weighted Raster')
+#Add it to the map layer registry ( display it on the map)
 QgsMapLayerRegistry.instance().addMapLayer(rasterVoronoi)
-#distanceGrids.append(distanceGrid)
 
+#polygonize the result raster
+os.system('gdal_polygonize.py "/home/user/Desktop/PyQGIS_Tests/Weighted Voronoi/rasterVoronoi.tiff" -f "ESRI Shapefile" "/home/user/Desktop/PyQGIS_Tests/Weighted Voronoi/shp/WeightedVoronoi/WeightedVoronoi.shp" WeightedVoronoi')
 
-#print "all cells with a weighted value"
+weightedVoronoiVector=QgsVectorLayer('./Desktop/PyQGIS_Tests/Weighted Voronoi/shp/WeightedVoronoi/WeightedVoronoi.shp', 'weighted voronoi', 'ogr')
+#load the vector weighted voronoi diagram
+QgsMapLayerRegistry.instance().addMapLayer(weightedVoronoiVector)
+# #print "all cells with a weighted value"
