447908f67c12b9762f46441848a37da09d0eda00
HichamZouarhi <hicham.zouarhi@gmail.com>
2016-09-25 19:59:56 +0100

    delete test data

--- script.py
+++ script.py
@@ -6,17 +6,17 @@ import os
 
 
 #Get the points vector layer
-pointsVector=QgsVectorLayer(sys.argv+'points.shp', 'points', 'ogr')
+pointsVector=QgsVectorLayer(sys.argv, 'points', 'ogr')
 #Add the vector layer to the map layer registry
 QgsMapLayerRegistry.instance().addMapLayer(pointsVector)
 
-os.system('gdal_rasterize -a z -ts 1000 1000 -l points "'+sys.argv+'/points.shp" "'+sys.argv+'/rasterPoints"')
+os.system('gdal_rasterize -a z -ts 1000 1000 -l points "'+sys.argv+'" "./rasterPoints"')
 
 
-#rasterPoints=QgsRasterLayer(sys.argv'/rasterPoints', 'rasterPoints')
+#rasterPoints=QgsRasterLayer('./rasterPoints', 'rasterPoints')
 #QgsMapLayerRegistry.instance().addMapLayer(rasterPoints)
 
-dataset=gdal.Open(sys.argv+'/rasterPoints')
+dataset=gdal.Open('./rasterPoints')
 numpy_array=dataset.ReadAsArray()
 
 width,height=numpy_array.shape
@@ -48,7 +48,7 @@ for row in range(0,height):
 
 #save the distance grid as an output raster
 #output file name ( path to where to save the raster file )
-outFileName=sys.argv+'/rasterVoronoi.tiff'
+outFileName='./rasterVoronoi.tiff'
 #call the driver for the chosen format from GDAL
 driver=gdal.GetDriverByName('GTiff')
 #Create the file with dimensions of the input raster ( rasterized points )
@@ -60,14 +60,14 @@ output.SetProjection(dataset.GetProjection())
 #insert data to the resulting raster in band 1 from the weighted distance grid
 output.GetRasterBand(1).WriteArray(distanceGrid)
 #Call the raster output file
-rasterVoronoi=QgsRasterLayer(sys.argv+'/rasterVoronoi.tiff', 'weighted Raster')
+rasterVoronoi=QgsRasterLayer('./rasterVoronoi.tiff', 'weighted Raster')
 #Add it to the map layer registry ( display it on the map)
 QgsMapLayerRegistry.instance().addMapLayer(rasterVoronoi)
 
 #polygonize the result raster
-os.system('gdal_polygonize.py "'+sys.argv+'/rasterVoronoi.tiff" -f "ESRI Shapefile" "'+sys.argv+'/WeightedVoronoi.shp" WeightedVoronoi')
+os.system('gdal_polygonize.py "./rasterVoronoi.tiff" -f "ESRI Shapefile" "./WeightedVoronoi.shp" WeightedVoronoi')
 
-weightedVoronoiVector=QgsVectorLayer(sys.argv+'/WeightedVoronoi.shp', 'weighted voronoi', 'ogr')
+weightedVoronoiVector=QgsVectorLayer('./WeightedVoronoi.shp', 'weighted voronoi', 'ogr')
 #load the vector weighted voronoi diagram
 QgsMapLayerRegistry.instance().addMapLayer(weightedVoronoiVector)
 # #print "all cells with a weighted value"
